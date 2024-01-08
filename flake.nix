e1adc2c2977d3602aaaf47a7b4a3b0f133d618dc
steinuil <steenuil.owl@gmail.com>
2024-01-08 13:00:56 +0100

    initial commit

---
+++ flake.nix
@@ -0,0 +1,63 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+    opam-repository = {
+      url = "github:ocaml/opam-repository";
+      flake = false;
+    };
+    opam-nix = {
+      url = "github:tweag/opam-nix";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        flake-utils.follows = "flake-utils";
+        opam-repository.follows = "opam-repository";
+      };
+    };
+  };
+  outputs = { self, nixpkgs, flake-utils, opam-repository, opam-nix }:
+    let package = "shuko";
+    in flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        on = opam-nix.lib.${system};
+        devPackagesQuery = {
+          ocaml-lsp-server = "*";
+          ocamlformat = "*";
+          utop = "*";
+        };
+        repos = [
+          opam-repository
+        ];
+        query = devPackagesQuery // {
+          ocaml-base-compiler = "5.1.1";
+        };
+        scope = on.buildDuneProject { inherit repos; } package ./. query;
+        overlay = final: prev: {
+          # You can add overrides here
+          ${package} = prev.${package}.overrideAttrs (_: {
+            # Prevent the ocaml dependencies from leaking into dependent environments
+            doNixSupport = false;
+          });
+        };
+        scope' = scope.overrideScope' overlay;
+        # The main package containing the executable
+        main = scope'.${package};
+        # Packages from devPackagesQuery
+        devPackages = builtins.attrValues
+          (pkgs.lib.getAttrs (builtins.attrNames devPackagesQuery) scope');
+      in
+      {
+        legacyPackages = scope';
+
+        packages.default = main;
+
+        devShells.default = pkgs.mkShell {
+          inputsFrom = [ main ];
+          buildInputs = devPackages ++ [
+            # You can add packages from nixpkgs here
+          ];
+        };
+      });
+}
+
